d4f21476320b2d789e7a7f4448cc98ca0edfbe5d
Gohil Harshil <gohilharshil858@gmail.com>
2025-02-07 11:56:04 +0530

    third commit

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const menuOpenButton = document.querySelector("#menu-open-button");
 menuOpenButton.addEventListener("click",() => {
     document.body.classList.toggle("show-mobile-menu");
 });
+
+menuCloseButton.addEventListener("click",() => {
+    menuOpenButton.click();
+})
